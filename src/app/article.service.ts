8de5c6a8725d6bd989caf1db8f6d37bca2dd46ed
Giulio <giglio_2@hotmail.it>
2020-12-19 00:22:41 +0100

    added mock list of articles

---
+++ src/app/article.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Article } from './article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor() { }
+}
---
+++ src/app/mockarticles.ts
@@ -0,0 +1,14 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+  { id: 1, title: 'Dr Nice', url: 'https://www.google.com' },
+  { id: 2, title: 'Narco', url: 'https://www.google.com'  },
+  { id: 3, title: 'Bombasto', url: 'https://www.google.com' },
+  { id: 4, title: 'Celeritas', url: 'https://www.google.com' },
+  { id: 5, title: 'Magneta', url: 'https://www.google.com' },
+  { id: 6, title: 'RubberMan', url: 'https://www.google.com' },
+  { id: 7, title: 'Dynama', url: 'https://www.google.com' },
+  { id: 8, title: 'Dr IQ', url: 'https://www.google.com' },
+  { id: 9, title: 'Magma', url: 'https://www.google.com' },
+  { id: 10, title: 'Tornado', url: 'https://www.google.com' }
+];
